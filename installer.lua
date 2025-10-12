a4c0593ac5d45ebc55209d614e3a5995f1038fc9
mallowwww <mallowhorton@proton.me>
2025-10-12 14:52:54 -0500

    Better installer script

--- installer.lua
+++ installer.lua
@@ -1,5 +1,6 @@
 -- Based on MCHTTP by HHOY
 local githubPage = "https://raw.githubusercontent.com/Mallowwww/CCHTTP-for-CCML/refs/heads/main/"
+local directory = "cchttp/"
 local files = {
     "client/mchttp.lua",
     "client/craftium.lua",
@@ -10,27 +11,26 @@ local files = {
     "server/mchttp-server.lua",
     "server/server.lua"
 }
+local categoryNames = {"Client", "DNS", "Server", "All", "Quit"}
 local categories = {
-    "Client": [
+    Client = {
         "client/mchttp.lua",
         "client/craftium.lua",
         "client/browser.lua",
         "client/dnsapi.lua"
-    ], "DNS": [
+    }, DNS = {
         "dns/dns.lua",
         "dns/lookup"
-    ], "Server": [
+    }, Server = {
         "server/mchttp-server.lua",
         "server/server.lua"
-    ], "All": files,
-    "Quit": []
+    }, All = files,
+    Quit = {}
 }
 print("Installing CCHTTP v0.0.1...")
 print("Available distrobutions:")
-local n = 1
-for i,a in ipairs(categories) do
-    print(n,i)
-    n = n + 1
+for i,a in pairs(categoryNames) do
+    print("    ", i, a)
 end
 write("Write number to install: ")
 local ans = nil
@@ -39,14 +39,18 @@ while true do
     if not ans or ans < 1 or ans > 5 then
         write("Please select a valid number: ")
     else
-        if ans == 5 then return end
+        if categoryNames[ans] == "Quit" then 
+            print("Exiting...")
+            return 
+        end
         break
     end
 end
-for i,j in pairs(categories[ans]) do
+for i,j in pairs(categories[categoryNames[ans]]) do
+    print(i, j, ans)
     local req = http.get(githubPage..j)
     local data = req.readAll()
-    local handle = fs.open(files[ans],"w")
+    local handle = fs.open(directory..j,"w")
     handle.write(data)
     handle.close()
 end
