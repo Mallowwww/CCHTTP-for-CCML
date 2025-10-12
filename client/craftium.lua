8248bc16f843a27e0fa073e99ba13bc182b5c9a8
mallowwww <mallowhorton@proton.me>
2025-10-12 15:32:25 -0500

    Attempt implement ccml

--- client/craftium.lua
+++ client/craftium.lua
@@ -1,9 +1,22 @@
 local api = {}
+local basalt = require("basalt")
 -- Based on MCHTTP by HHOY
-function api.startInstance(siteData,terminal,mchttp)
-    local tX,tY = terminal.getSize()
+function crawlForNode(node, name)
+    if not node then return nil end
+    if node.ni == name then return node end
+    if node.children then
+        for i,j in pairs(node.children) do
+            result = crawlForNode(j, name)
+            if result then return result end
+        end
+    end
+    return nil
+end
+function api.startInstance(siteData, frame, mchttp)
+    api.frame = frame
+    local tX,tY = frame:getSize()
     local env = {
-        term = window.create(terminal,1,2,tX,tY),
+        frame = api.frame,
         os = {
             pullEvent = os.pullEvent,
             queueEvent = os.queueEvent,
@@ -23,12 +36,34 @@ function api.startInstance(siteData,terminal,mchttp)
     if mchttp then
         env.mchttp = mchttp
     end
-    local func, err = load(siteData,"site",nil,env)
-    if err then
-        error(err)
-    else
-        func()
+    --local func, err = load(siteData,"site",nil,env)
+    local xml = basalt.getAPI("xml")
+    if not xml then
+        print("ERROR - XML plugin not found")
+        return
     end
+    local parsed = xml.parseText(siteData)
+    if not parsed then
+        print("ERROR - Can't parse xml")
+        return
+    end
+    local customEnv = crawlForNode("env")
+    if customEnv and textutils.unserialise(customEnv) then
+        for i,j in customEnv do
+            if not env[i] then
+                env[i] = j
+            end
+        end
+    else if customEnv and textutils.unserialise("{"..customEnv.."}") then
+        customEnv = "{"..customEnv.."}"
+        for i,j in customEnv do
+            if not env[i] then
+                env[i] = j
+            end
+        end
+    end
+    api.frame:loadXML(siteData, env)
+    
 end
 
 return api
