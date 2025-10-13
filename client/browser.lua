0dad77773a5214fcd824c4eb1e89ca8f3d967cc5
mallowwww <mallowhorton@proton.me>
2025-10-12 23:58:17 -0500

    Add address bar

--- client/browser.lua
+++ client/browser.lua
@@ -8,6 +8,20 @@ if type(arg[1]) ~= "string" then
 end
 local state = {}
 state.frame = basalt.getMainFrame()
+function addressBarWidget(frame)
+    local widget = frame:addContainer()
+        :setWidth("{parent.width}")
+        :setHeight(1)
+        :setBackground(colors.gray)
+    return widget
+end
+function browserFrameWidget(data, frame, mchttp)
+    local widget = frame:addContainer()
+        :setWidth("{parent.width}")
+        :setHeight("{parent.height - 1}")
+        :setY(2)
+    craftium.startInstance(data, widget, mchttp)
+end
 if fs.exists(arg[1]) then
     
     local handle = fs.open(arg[1],"r")
@@ -21,7 +35,8 @@ else
         print(addr)
         local result = mchttp.request(addr,80,nil,"/","GET",5)
         if result.status == 176 then error(result.body,0) end
-        craftium.startInstance(result.body,state.frame,mchttp)
+        local browserFrame = browserFrameWidget(result.body,state.frame,mchttp)
+        local addressBar = addressBarWidget(state.frame)
         basalt.run()
     else
         error(err,0)
