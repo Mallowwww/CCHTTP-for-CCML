45c809aa3377633b2e6921c7c8733c0d31a31813
mallowwww <mallowhorton@proton.me>
2025-10-15 18:04:30 -0500

    Wildcards work now

--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,4 @@
+-- Example server file
 local cchttpserver = require("cchttp-server")
 
 local app = cchttpserver.new(80)
@@ -14,9 +15,9 @@ end)
 app:listen("/*", "GET", function(pack)
     print("Wildcard hit! ", pack.body)
     if pack.path[-1] == "/" then pack.path = string.sub(pack.path, 1, #pack.path) end
-    local location = directory..pack.path.."index.ccml", "r"
-    if not fs.exists(location) then return end
-    local handle = fs.open(location)
+    local location = directory.."/"..pack.path..".ccml", "r"
+    if not fs.exists(location) then return {body="404",contentType="text/plain"} end
+    local handle = fs.open(location, "r")
     local data = handle.readAll()
     handle.close()
     return {body=data,contentType="text/plain"}
